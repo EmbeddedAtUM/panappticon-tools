61f661b15c5173697ce3afcd28344954b322d757
lide <lide@gambit.(none)>
2012-09-19 12:31:24 -0400

    Continue adding

---
+++ simple-apps/EventLoggingTest/src/www/EventLoggingTest/EventLoggingTestActivity.java
@@ -0,0 +1,57 @@
+package www.EventLoggingTest;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class EventLoggingTestActivity extends Activity {
+	
+	private String TAG = "EventLoggingTestActivity";
+	
+	private Button mThreadStartButton;
+	private CpuExerciser mCpuExerciser;
+	
+	 final Runnable mUpdateButton = new Runnable() {
+	        public void run() {
+	        	Log.d(TAG,"****In the update runnable");
+	            updateButton();
+	        }
+	    };
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mCpuExerciser = new CpuExerciser(this);
+        
+        mThreadStartButton = (Button)findViewById(R.id.threadStartButton);
+        mThreadStartButton.setOnClickListener(threadStartButtonListener);
+        mThreadStartButton.setText("Start CPU exerciser!");
+        mThreadStartButton.setBackgroundColor(Color.GREEN);
+    }
+    
+    public void onStart(){
+    	super.onStart();
+    
+    }
+    private Button.OnClickListener threadStartButtonListener =
+        new Button.OnClickListener() {
+          public void onClick(View v) {
+            mThreadStartButton.setEnabled(false);
+            mThreadStartButton.setText("CPU exerciser working!");
+            mThreadStartButton.setBackgroundColor(Color.RED);
+            Thread t1 = new Thread(mCpuExerciser);
+            t1.start();
+            mThreadStartButton.setEnabled(true);
+          }
+      };
+      
+      public void updateButton(){
+      	mThreadStartButton.setText("CPU exercise has stopped");
+  		mThreadStartButton.setBackgroundColor(Color.YELLOW);
+      }
+    
+}
